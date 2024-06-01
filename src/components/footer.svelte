9f5a3252252003931f6ee5be440fac48947932dd
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2024-06-01 23:24:35 +0200

    [FEAT] - add copyright

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -3,21 +3,24 @@
 </script>
 
 <section class="my-8">
-	<h2 class="text-h2 font-serif mb-4">Pour discuter</h2>
-	<div class="flex flex-col gap-2 md:flex-row md:flex-wrap">
+	<h2 class="mb-4 font-serif text-h2">Pour discuter</h2>
+	<div class="flex flex-col gap-4 md:flex-row md:flex-wrap">
 		<a
-			class="underline text-blue flex gap-2 items-center"
+			class="flex gap-2 items-center underline text-blue"
 			href="https://www.instagram.com/emma_glme/"
 		>
 			<Icon icon="ph:instagram-logo-light" />
 			@emma_glme</a
 		>
 		<a
-			class="underline text-blue flex gap-2 items-center"
+			class="flex gap-2 items-center underline text-blue"
 			href="https://www.linkedin.com/in/emma-guillaume-7122a6202/"
 		>
 			<Icon icon="basil:linkedin-solid" />
 			Emma Guillaume
 		</a>
 	</div>
+</section>
+<section class="my-8">
+	<p>©2024 Emma Guillaume - Tous droits réservés</p>
 </section>
